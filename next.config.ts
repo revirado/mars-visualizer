f9f8cad8fc615e4d2fc533592d0b6152bc452aa7
revirado <parallel.qiller@gmail.com>
2025-10-07 22:00:50 -0300

    set allowed Dev Origins, next configs

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,13 @@ const nextConfig: NextConfig = {
     domains: [
       "d2pn8kiwq2w21t.cloudfront.net","mars.nasa.gov","img.icons8.com"
     ]
-  }
+  },
+// permite orígenes de dev (ajusta la IP a la que te muestra Next)
+    allowedDevOrigins: [
+    "http://192.168.100.12:3000",
+    "http://192.168.100.13:3000"
+  ],
 };
+
 
 export default nextConfig;
